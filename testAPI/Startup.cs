72168f959c331bcdc7e56308bfd23e287f20a1ea
Vivek Rokkam <vivekrokkam@gmail.com>
2019-08-25 19:58:22 +1200

    cors fix

--- testAPI/Startup.cs
+++ testAPI/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using testAPI.Model;
 using Swashbuckle.AspNetCore.Swagger;
+
+
 namespace testAPI
 {
     public class Startup
@@ -26,6 +28,7 @@ namespace testAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
             services.AddDbContext<REEviewContext>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddSwaggerGen(c =>
@@ -37,6 +40,13 @@ namespace testAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors(builder =>
+            {
+                builder.WithOrigins("https://reeviewit.azurewebsites.net")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials();
+            });
             app.UseSwagger();
 
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
